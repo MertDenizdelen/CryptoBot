5f444f2e9942ac29fd555e9ccdbd234f060d5183
Rick van der waal <rickwaal@hotmail.com>
2018-02-01 21:47:59 +0100

    Moved arbitrage detection to separate controller

---
+++ ArbitrageDetector/Controllers/ArbitrageController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArbitrageDetector.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace ArbitrageDetector.Controllers
+{
+    [Route("api/[controller]")]
+    public class ArbitrageController : Controller
+    {
+        private readonly ArbitrageConfiguration _config;
+        private readonly IInMemoryPriceRepository _priceRepository;
+
+        public ArbitrageController(IInMemoryPriceRepository priceRepository, ArbitrageConfiguration config)
+        {
+            _config = config;
+            _priceRepository = priceRepository;
+        }
+
+        [HttpGet]
+        public List<ArbitrageModel> GetArbitrages()
+        {
+            var arbitrages = new List<ArbitrageModel>();
+            var prices = _priceRepository.GetCopy();
+
+            foreach (var buyModel in prices)
+            {
+                foreach (var sellModel in prices)
+                {
+                    // Different market but same currency pair and the price datetime difference is less than X. 
+                    if (buyModel.Market != sellModel.Market
+                        && buyModel.From == sellModel.From
+                        && buyModel.To == sellModel.To
+                        && Math.Abs((buyModel.DateTime - sellModel.DateTime).TotalSeconds) < _config.AllowedPriceTimeDifferenceInSeconds)
+                    {
+                        // Calculate the difference when buying on the buyModel and selling on the sellModel. 
+                        var amount = 10;
+                        var buyFee = 1.99m; // TODO: calculate fee
+                        var cost = amount * buyModel.BuyPrice + buyFee;
+                        var sellFee = 1.99m; // TODO: calculate fee
+                        var yield = amount * sellModel.SellPrice - sellFee;
+
+                        var profitPercentage = (yield - cost) / yield * 100;
+
+                        arbitrages.Add(new ArbitrageModel
+                        {
+                            BuyModel = buyModel,
+                            SellModel = sellModel,
+                            Percentage = profitPercentage,
+                            DateTime = DateTime.Now
+                        });
+                    }
+                }
+            }
+
+            return arbitrages;
+        }
+    }
+}
--- ArbitrageDetector/Controllers/PriceController.cs
+++ ArbitrageDetector/Controllers/PriceController.cs
@@ -12,13 +12,11 @@ namespace ArbitrageDetector.Controllers
     public class PriceController : Controller
     {
         private readonly ILogger<PriceController> _logger;
-        private readonly ArbitrageConfiguration _config;
         private readonly IInMemoryPriceRepository _priceRepository;
 
-        public PriceController(ILogger<PriceController> logger, ArbitrageConfiguration config, IInMemoryPriceRepository priceRepository)
+        public PriceController(ILogger<PriceController> logger, IInMemoryPriceRepository priceRepository)
         {
             _logger = logger;
-            _config = config;
             _priceRepository = priceRepository;
         }
 
@@ -27,46 +25,6 @@ namespace ArbitrageDetector.Controllers
         {
             _priceRepository.Add(price);
             _logger.LogInformation(price.ToString());
-        }
-
-        [HttpGet]
-        [Route("/arbitrages")]
-        public List<ArbitrageModel> GetArbitrages()
-        {
-            var arbitrages = new List<ArbitrageModel>();
-            var prices = _priceRepository.GetCopy();
-
-            foreach (var buyModel in prices)
-            {
-                foreach (var sellModel in prices)
-                {
-                    // Different market but same currency pair and the price datetime difference is less than X. 
-                    if (buyModel.Market != sellModel.Market
-                        && buyModel.From == sellModel.From
-                        && buyModel.To == sellModel.To
-                        && Math.Abs((buyModel.DateTime - sellModel.DateTime).TotalSeconds) < _config.AllowedPriceTimeDifferenceInSeconds)
-                    {
-                        // Calculate the difference when buying on the buyModel and selling on the sellModel. 
-                        var amount = 10;
-                        var buyFee = 1.99m; // TODO: calculate fee
-                        var cost = amount * buyModel.BuyPrice + buyFee;
-                        var sellFee = 1.99m; // TODO: calculate fee
-                        var yield = amount * sellModel.SellPrice - sellFee;
-
-                        var profitPercentage = (yield - cost) / yield * 100;
-
-                        arbitrages.Add(new ArbitrageModel
-                        {
-                            BuyModel = buyModel,
-                            SellModel = sellModel,
-                            Percentage = profitPercentage,
-                            DateTime = DateTime.Now
-                        });
-                    }
-                }
-            }
-
-            return arbitrages;
         }
     }
 }
